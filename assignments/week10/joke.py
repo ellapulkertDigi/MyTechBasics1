3e13abb85b0a76bf16cc5b5aa858b5244698aec9
ellapulkertDigi <ella.pulkert@icloud.com>
2025-06-12 11:13:05 +0200

    making some cow jokes

--- assignments/week10/joke.py
+++ assignments/week10/joke.py
@@ -1,4 +1,15 @@
 import pyjokes
-print(pyjokes.get_joke())
 import cowsay
-cowsay.cow(pyjokes.get_joke())
+import jokevariance
+
+kind_of_joke = input("Do you want to hear a kinda good joke or a kinda bad joke? Please type 'good' or 'bad': ").strip().lower()
+
+if kind_of_joke == "good":
+    cowsay.cow(pyjokes.get_joke())
+elif kind_of_joke == "bad":
+    cowsay.cow(jokevariance.dadjoke)
+else:
+    print("Now you won't hear any type of joke ):")
+
+
+
